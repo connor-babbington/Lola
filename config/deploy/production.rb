e31c9fca82f36e94350922ba9a3988e767e0db38
Connor Babbington <connorbabbington@warwickshire.gov.uk>
2018-09-07 03:35:52 -0700

    first commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,7 @@
 set :branch, ENV.fetch("CAPISTRANO_BRANCH", "master")
 set :mb_sidekiq_concurrency, 1
 
-server "lola.xn--facebok-i0a.com",
-       user: "deployer",
+server "178.128.166.176",
+       user: "deploy",
        roles: %w[app backup cron db redis sidekiq web]
+set :deploy_to, "/var/www/lola/live"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,9 @@
 set :branch, ENV.fetch("CAPISTRANO_BRANCH", "development")
 set :mb_sidekiq_concurrency, 1
+set :stage, :staging
+set :rails_env, :staging
 
-server "staging.xn--facebok-i0a.com",
-       user: "deployer",
+server "178.128.166.176",
+       user: "deploy",
        roles: %w[app backup cron db redis sidekiq web]
+set :deploy_to, "/var/www/lola/staging/"
